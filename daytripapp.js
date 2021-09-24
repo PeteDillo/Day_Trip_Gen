df2b1871758b094fe5c87d2f96dc5f8efedbc532
unknown <pedelgadillo@gmail.com>
2021-09-24 14:49:01 -0500

    finished up on the loop breaking the reselector works perfectlystaying with if else statements. Started working on the rest of the trip to be done with the functions.

--- daytripapp.js
+++ daytripapp.js
@@ -11,47 +11,54 @@ restaurant = rndArray(resArray)
 transport = rndArray(transArray)
 entertainment = rndArray(enteArray)
 
-changeYesNo = prompt("Your destination is "+ destination + ". You'll get there via " + transport + ". Eating a wonderful local " + restaurant + ' restaurant. After that enjoy a night enjoyinging a ' + entertainment + '. Do you want to change anything?')
+changeYesNo = prompt("Your destination is "+ destination + ". You'll get there via " + transport + ". Eating a wonderful local " + restaurant + ' restaurant. After that a night enjoyinging a ' + entertainment + '. Do you want to change anything?')
 
 
-if(changeYesNo === 'Y' || "yes" || 'Yes' || 'y'){
-    let desAgree = agreeCheck(prompt("Do you want to change your destination?"), destination,)
-    if (desAgree === 'Y' || "yes" || 'Yes' || 'y' ){
-        reSelector(destination,desArray)
-    }
-
+if(changeYesNo ==='y' || changeYesNo === 'Y' ||changeYesNo === "yes" || changeYesNo === 'Yes'){
+    let desAgree = agreeCheck(prompt("Do you want to change " + destination + "?"), destination, desArray)   
+    let transAgree = agreeCheck(prompt("Do you want to change " + transport + "?"), transport, transArray)
+    let resAgree = agreeCheck(prompt("Do you want to change " + restaurant + "?"), restaurant, resArray)
+    let enteAgree = agreeCheck(prompt("Do you want to change " + entertainment + "?"), entertainment, enteArray)
 }
-//if else(changeYesNo ==="N" || "n" || 'No' || 'no'){}
+
+
+//if else(changeYesNo === "N" || === "n" || === 'No' || ==='no'){}
 
 
 // function for random array
-function rndArray(Array){
-    let randomNumber =  Math.floor(Math.random() * Array.length)
-    return Array[randomNumber]
+function rndArray(typeArray){
+    let randomNumber =  Math.floor(Math.random() * typeArray.length)
+    return typeArray[randomNumber]
 }
 //function to check if we like the random item
-function agreeCheck(agree, item){
-    agree = prompt("Do you want to change  " + item + "?")
-    if(agree ==='Y' || "yes" || 'Yes' || 'y' ){   
-        return agree;
+function agreeCheck(agree, item, typeArray){
+    if(agree ==='y' || agree === 'Y' ||agree === "yes" || agree === 'Yes'){   
+        reSelector(item, typeArray)
     }
 
 }
 // function to run reselector 
-function reSelector(item, typeArray)
-    let agreeToChange = "Y"
-    if(agreeToChange === 'Y' || "yes" || 'Yes' || 'y'){
-        rndArray(typeArray)
+function reSelector(item, typeArray){
+    let agreeToChange = 'y'
+
+    if (agreeToChange ==='y' || agreeToChange === 'Y' ||agreeToChange === "yes" || agreeToChange === 'Yes' ){
+        item = rndArray(typeArray)
+        agreeToChange = 'n'
         agreeToChange = prompt('Your choice of ' + item + ' has been saved. Would you like to change your choice?' )
-        return agreeToChange;
+
+        if (agreeToChange==='y'){
+            reSelector(item, typeArray)
+
+        }
+        
     }
-    else if (agree ==='No' || 'no' || 'n'|| 'N'){
-        return
+    else if (agree === "N" || agree === "n" || agree === 'No' || agree ==='no'){
+        return item
 }
     else{
     alert('Error invaild Please insert either Yes or No restarting')
     reSelector(item, typeArray)
 }
-
+}
 //  End Confirmation Alert
 alert("Your order for "+ destination + " via " + transport + ", dinning in at a " + restaurant + ' restaurant. and a ' + entertainment + ' is complete. Have a nice day!')
